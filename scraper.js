6f002df03b8c42f20b075924adbda02e83b9a02c
Nicholas Hwang <nick.joosung.hwang@gmail.com>
2013-09-04 13:51:40 -0400

    Parse each language uri and check for filelink

--- scraper.js
+++ scraper.js
@@ -4,9 +4,36 @@ var cheerio = require('cheerio'),
 
 var url = "http://learnxinyminutes.com";
 
+var once = 0;
+
 request(url, function(err, resp, body){
     if (err) throw err;
     $ = cheerio.load(body);
     var language = $('.container').children('table').first();
-    console.log(language.find('.name').text().trim());
+
+    language.find('tr').each(function(){
+      var lang = $(this).find('td').first();
+      var lang_text = lang.text().trim();
+      if (lang.text() != 0){
+        var lang_url = url + lang.find('a').attr('href');
+
+        if (once < 3){ // DEBUG
+          request(lang_url, function(err, resp, body){
+            if (err) throw err;
+            $ = cheerio.load(body);
+            if ($('.filelink').length) {
+              console.log("Link found for "+lang_text);
+              // console.log($('.filelink a').attr('href') + ":" + $('.filelink a').text());
+              request($('.filelink').attr('href')).pipe(fs.createWriteStream($('.filelink a').text()));
+            } else {
+              console.log("Link not found for "+lang_text);
+              // fs.writeFile('languages/'+lang_text, lang_text,function(err) {
+              //   if (err) throw err;
+              // });
+            }
+          });
+          once = ++once;
+        }
+      }
+    });
 });
