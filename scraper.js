e4a2766145cbf524a5546bafe21419fd8c264cf4
Nicholas Hwang <nick.joosung.hwang@gmail.com>
2013-09-04 14:15:27 -0400

    Add directory check and maker

--- scraper.js
+++ scraper.js
@@ -3,37 +3,44 @@ var cheerio = require('cheerio'),
     fs      = require('fs');
 
 var url = "http://learnxinyminutes.com";
+var languages_dir = "languages";
 
-var once = 0;
 
-request(url, function(err, resp, body){
-    if (err) throw err;
-    $ = cheerio.load(body);
-    var language = $('.container').children('table').first();
-
-    language.find('tr').each(function(){
-      var lang = $(this).find('td').first();
-      var lang_text = lang.text().trim();
-      if (lang.text() != 0){
-        var lang_url = url + lang.find('a').attr('href');
-
-        if (once < 3){ // DEBUG
-          request(lang_url, function(err, resp, body){
-            if (err) throw err;
-            $ = cheerio.load(body);
-            if ($('.filelink').length) {
-              console.log("Link found for "+lang_text);
-              // console.log($('.filelink a').attr('href') + ":" + $('.filelink a').text());
-              request($('.filelink').attr('href')).pipe(fs.createWriteStream($('.filelink a').text()));
-            } else {
-              console.log("Link not found for "+lang_text);
-              // fs.writeFile('languages/'+lang_text, lang_text,function(err) {
-              //   if (err) throw err;
-              // });
-            }
-          });
-          once = ++once;
-        }
-      }
+fs.stat(languages_dir, function(err,stats){
+  if (err){
+    fs.mkdir(languages_dir,0777,function(err){
+      if (err) throw err
     });
+  }
 });
+
+// var once = 0;
+
+// request(url, function(err, resp, body){
+//     if (err) throw err;
+//     $ = cheerio.load(body);
+//     var language = $('.container').children('table').first();
+
+//     language.find('tr').each(function(){
+//       var lang = $(this).find('td').first();
+//       var lang_text = lang.text().trim();
+//       if (lang.text() != 0){
+//         var lang_url = url + lang.find('a').attr('href');
+
+//         if (once < 3){ // DEBUG
+//           request(lang_url, function(err, resp, body){
+//             if (err) throw err;
+//             $ = cheerio.load(body);
+//             // if ($('.filelink').length) {
+//             //   request(url+$('.filelink a').attr('href')).pipe(fs.createWriteStream('languages/'+$('.filelink a').text()));
+//             // } else {
+//             //   fs.writeFile('languages/'+lang_text, $('pre.highlight'),function(err) {
+//             //     if (err) throw err;
+//             //   });
+//             // }
+//           });
+//           once = ++once;
+//         }
+//       }
+//     });
+// });
